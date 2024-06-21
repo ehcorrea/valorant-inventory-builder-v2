5c26d48a37ef45e7f5c372749b8bcb82be6e6774
Emilio H. Correa <ehcorrea60@gmail.com>
2024-06-21 20:30:35 -0300

    chore: add eslint, prettier and editorconfig rules

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: [
+    'expo',
+    'prettier',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'no-console': 'warn',
+  },
+};
