be8cbd441ef400b6b0bee3cc9d2e8207adf78642
Emilio H. Correa <ehcorrea60@gmail.com>
2024-06-23 17:25:45 -0300

    feat: add QueryClientProvider

--- src/components/Provider/Provider.tsx
+++ src/components/Provider/Provider.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from 'styled-components/native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import theme from '../../constants/theme';
 
@@ -6,6 +7,12 @@ type ProviderProps = {
   children: React.ReactElement;
 };
 
+const queryClient = new QueryClient();
+
 export function Provider({ children }: ProviderProps) {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </QueryClientProvider>
+  );
 }
