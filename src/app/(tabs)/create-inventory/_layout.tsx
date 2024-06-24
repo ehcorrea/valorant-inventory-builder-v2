bde80f9e17ac1d570beeda858c5111bb5c05c0dd
Emilio H. Correa <ehcorrea60@gmail.com>
2024-06-23 21:01:11 -0300

    feat: init create inventory routes

--- src/app/(tabs)/create-inventory/_layout.tsx
+++ src/app/(tabs)/create-inventory/_layout.tsx
@@ -1,37 +1,15 @@
-import { useCallback } from 'react';
-import { View } from 'react-native';
 import { Stack } from 'expo-router';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-
-import { Provider } from '@/components';
-
-SplashScreen.preventAutoHideAsync();
 
 export default function LayoutStack() {
-  const [fontsLoaded, fontError] = useFonts({
-    'ShareTech-Regular': require('@/assets/fonts/ShareTech-Regular.ttf'),
-    'Valorant-Font': require('@/assets/fonts/Valorant-Font.ttf'),
-    Dinnextw1g: require('@/assets/fonts/Dinnextw1g.otf'),
-  });
-
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded || fontError) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError]);
-
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
-
   return (
-    <Provider>
-      <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="index" />
-        </Stack>
-      </View>
-    </Provider>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen
+        name="skins"
+        options={{
+          presentation: 'modal',
+        }}
+      />
+    </Stack>
   );
 }
--- src/screens/CreateInventoryScreen/CreateInventoryScreen.tsx
+++ src/screens/CreateInventoryScreen/CreateInventoryScreen.tsx
@@ -1,11 +1,14 @@
 import { View } from 'react-native';
 
 import { Text } from '@/components';
+import { Link } from 'expo-router';
 
 export function CreateInventoryScreen() {
   return (
-    <View style={{ flex: 1 }}>
-      <Text>CreateInventoryScreen</Text>
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <Link href="/create-inventory/skins">
+        <Text>CreateInventoryScreen</Text>
+      </Link>
     </View>
   );
 }
