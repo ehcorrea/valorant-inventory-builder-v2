89cb0b0e943537f14bb290fd125dc1ca86a90b67
Emilio H. Correa <ehcorrea60@gmail.com>
2024-06-29 16:11:06 -0300

    feat: remove weapons api and add weapon to skin endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,34 +1,17 @@
-import { Weapon } from '@/types/weapon';
+import { Skin } from '@/types/weapon';
 
 import { client } from './client';
 
-export type WeaponsResponse = {
-  data: {
-    attributes: Weapon;
-  }[];
-};
-
-export async function getWeapons() {
-  const { data } = await client.get<WeaponsResponse>(`/weapons`);
-  return data;
-}
-
-export type SkinsResponse = {
-  chromas: Exclude<SkinsResponse[], 'chromas'>;
-  displayIcon?: string;
-  displayName: string;
-  fullRender: string;
-  streamedVideo?: string;
-  swatch?: string;
-  uuid: string;
-}[];
+export type SkinsResponse = Skin[];
 
 export type SkinsRequest = {
   uuid: string;
 };
 
 export async function getSkins({ uuid }: SkinsRequest) {
-  const { data } = await client.get<SkinsResponse>(`/skins?uuid=${uuid}`);
+  const { data } = await client.get<SkinsResponse>(
+    `/weapon/skins?uuid=${uuid}`
+  );
 
   return data;
 }
