f4b28881b9a7f686295761d4ba38dc4282b8e83e
Emilio H. Correa <ehcorrea60@gmail.com>
2024-06-30 16:09:24 -0300

    feat: add layout to create-inventory index

--- src/routes/(tabs)/create-inventory/_layout.tsx
+++ src/routes/(tabs)/create-inventory/_layout.tsx
@@ -1,15 +1,23 @@
+import { Layout } from '@/components/Layout/Layout';
 import { Stack } from 'expo-router';
 
 export default function LayoutStack() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen
-        name="skins"
-        options={{
-          presentation: 'modal',
+    <Layout>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: undefined },
         }}
-      />
-    </Stack>
+      >
+        <Stack.Screen name="index" />
+        <Stack.Screen
+          name="skins"
+          options={{
+            presentation: 'modal',
+          }}
+        />
+      </Stack>
+    </Layout>
   );
 }
