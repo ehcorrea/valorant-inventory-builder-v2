471f444d95b37639a116399f16377016ca5f287b
Emilio H. Correa <ehcorrea60@gmail.com>
2024-06-30 12:22:06 -0300

    feat: add ButtonSpray to components index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export * from './ButtonPlayerCard/ButtonPlayerCard';
+export * from './ButtonSpray/ButtonSpray';
 export * from './ButtonValorant/ButtonValorant';
 export * from './ButtonWeapon/ButtonWeapon';
 export * from './Provider/Provider';
