a7b19da1714151fe285ea86143aa17439a9001e6
Emilio H. Correa <ehcorrea60@gmail.com>
2024-07-03 18:20:13 -0300

    feat: add Modal to component index

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ export * from './ButtonSpray/ButtonSpray';
 export * from './ButtonValorant/ButtonValorant';
 export * from './ButtonWeapon/ButtonWeapon';
 export * from './Layout/Layout';
+export * from './Modal/Modal';
 export * from './Provider/Provider';
 export * from './Text/Text';
