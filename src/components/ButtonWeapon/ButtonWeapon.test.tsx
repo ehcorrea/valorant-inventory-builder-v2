4db7eb45739343fd782b816b6b8b68ed58d76e6b
Emilio H. Correa <ehcorrea60@gmail.com>
2024-07-04 18:52:46 -0300

    feat: add variants to ButtonWeapon

--- src/components/ButtonWeapon/ButtonWeapon.test.tsx
+++ src/components/ButtonWeapon/ButtonWeapon.test.tsx
@@ -1,34 +1,44 @@
 import { fireEvent, render } from '@/test/utils';
 
-import { ButtonWeapon } from './ButtonWeapon';
+import { ButtonWeapon, ButtonWeaponProps } from './ButtonWeapon';
 
 const mockedOnPress = jest.fn();
 
-const setup = () => {
+const setup = (props: Partial<ButtonWeaponProps>) => {
   return render(
     <ButtonWeapon
+      {...(props as ButtonWeaponProps)}
       onPress={mockedOnPress}
       image="placeholder-image.png"
       testID="weapon-button"
+      displayName="Weapon Display Name"
     />
   );
 };
 
-describe('given that the component is rendered', () => {
-  test('render image passed by props', () => {
-    const container = setup();
-    expect(
-      container
-        .getByTestId('weapon-button')
-        .findByProps({ source: 'placeholder-image.png' })
-    ).toBeTruthy();
-    expect(container).toMatchSnapshot();
+describe('given that the <ButtonWeapon> is rendered', () => {
+  describe("and is 'weapon' variant", () => {
+    test('pressing calls a callback', () => {
+      const container = setup({ variant: 'weapon' });
+      const button = container.getByTestId('weapon-button');
+      fireEvent.press(button);
+      expect(mockedOnPress).toHaveBeenCalled();
+    });
+    test('render image and dont render label', () => {
+      const container = setup({ variant: 'weapon' });
+      const image = container.getByLabelText('Weapon Display Name button');
+      expect(image.props.source).toBe('placeholder-image.png');
+      expect(container.queryByText('Weapon Display Name')).toBeFalsy();
+      expect(container).toMatchSnapshot();
+    });
   });
-
-  test('pressing calls a callback', () => {
-    const container = setup();
-    const button = container.getByTestId('weapon-button');
-    fireEvent.press(button);
-    expect(mockedOnPress).toHaveBeenCalled();
+  describe("and is 'weapon-skin' variant ", () => {
+    test('render label', () => {
+      const container = setup({
+        variant: 'weapon-skin',
+      });
+      expect(container.getByText('Weapon Display Name')).toBeTruthy();
+      expect(container).toMatchSnapshot();
+    });
   });
 });
