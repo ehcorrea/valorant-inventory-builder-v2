5f942b588f98e1e719789701863e5509040d47ed
Emilio H. Correa <ehcorrea60@gmail.com>
2024-07-18 10:29:10 -0300

    feat: update ButtonSpray styles

--- src/components/ButtonSpray/ButtonSpray.styles.ts
+++ src/components/ButtonSpray/ButtonSpray.styles.ts
@@ -1,16 +1,21 @@
 import { TouchableOpacity } from 'react-native';
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import { Image, ImageProps } from 'expo-image';
 
 export const Button = styled(TouchableOpacity)`
-  max-width: 250px;
-  max-height: 250px;
-  aspect-ratio: 1/1;
+  ${({ theme }) => css`
+    aspect-ratio: 1/1;
+    background-color: #cfcfcf11;
+    border-radius: ${theme.border.radius.medium}px;
+    max-height: 250px;
+    max-width: 250px;
+    flex: 1;
+  `}
 `;
 
 export const ImageSpray = styled(Image).attrs<ImageProps>({
   contentFit: 'cover',
 })`
-  height: 100%;
-  width: 100%;
+  height: 90%;
+  width: 90%;
 `;
